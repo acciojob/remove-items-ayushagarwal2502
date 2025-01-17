382af945695404a862b1aa4e13dc77a1f9e09ee9
ayushagarwal2502 <159728868+ayushagarwal2502@users.noreply.github.com>
2025-01-17 22:59:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+function deletes(){
+    let list = document.getElementById("colorSelect");
+    let selectedOption = list.options[list.selectedIndex];
+    list.removeChild(selectedOption);
+}
